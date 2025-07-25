045c34cbda68aa145cf20f45ba029476631ef7d0
santhosh <santhoshchekuri2002@gmail.com>
2025-07-25 22:43:29 +0530

    Modified

--- frontend/src/app/auth.guard.ts
+++
@@ -1,6 +0,0 @@
-import { CanActivateFn } from '@angular/router';
-
-export const authGuard: CanActivateFn = (route, state) => {
-  return sessionStorage.getItem('ust.auth') !== null;
-  state.url = '/home';
-};
--- frontend/src/app/model/UserCredentials.ts
+++
@@ -1,6 +0,0 @@
-export interface UserCredentials{
-    username:string,
-    password:string,
-    role:string,
-    getId:number
-}
